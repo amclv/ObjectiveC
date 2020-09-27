159b2d170ee61f2be0704338f8f1cfb845330791
Aaron <toscleveland@gmail.com>
2020-09-26 20:56:48 -0500

    “ClassAnatomy done”

--- ClassAnatomy/ClassAnatomy/ViewController.m
+++ ClassAnatomy/ClassAnatomy/ViewController.m
@@ -7,16 +7,29 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
-
-@end
+// Not needed but only need if accessing the private property.
+//@interface ViewController ()
+//
+//@property (nonatomic, strong) NSString *vehicle;
+//
+//@end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    NSLog(@"Name: %@", self.name);
+    
+    self.name = @"Jack";
+    NSLog(@"Name: %@", self.name);
+    
+    _name = @"Peter";
+    NSLog(@"Name: %@", _name);
+    
+    [self setName:@"Sylvia"];
+    NSLog(@"Name: %@", [self name]);
 }
-
 
 @end
---
+++ ClassAnatomy/README.md
@@ -0,0 +1,12 @@
+.h header file for definition
+.m main file for implementation
+
+@interface is a class
+@end definition would end for the class. 
+
+defining @property in the .m file will make it only accessible by the
+class. 
+
+defining @property in the .h file will allow publically available
+ 
+
